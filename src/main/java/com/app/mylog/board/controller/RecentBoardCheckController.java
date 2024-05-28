3b54862fb46e39e903a25ab5111cbbc70ab7686d
JunSeo7 <163373037+JunSeo7@users.noreply.github.com>
2024-05-28 17:37:10 +0900

    Add files via upload

---
+++ src/main/java/com/app/mylog/board/controller/RecentBoardCheckController.java
@@ -0,0 +1,40 @@
+package com.app.mylog.board.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.app.mylog.board.service.BoardService;
+import com.app.mylog.board.vo.BoardVo;
+
+@WebServlet("/recent")
+public class RecentBoardCheckController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		try {
+			
+			BoardService bs = new BoardService();
+			List<BoardVo> voList = bs.recentBoardCheck();
+			
+			PrintWriter out = resp.getWriter();
+			out.write("voList : " + voList);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
